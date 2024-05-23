d3d71ef7ef1a7e8aa612f363c2df0f60cc69af4b
Lincoln <163782652+ballooxn@users.noreply.github.com>
2024-05-23 14:55:34 -0500

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if (typeof(n1) != 'number' || typeof(n2) != 'number') {
+        return 'ERROR';
+    }
+    if (n1 < 0 || n2 < 0) {
+        return 'ERROR';
+    }
 
+    finalSum = 0;
+    if (n1 > n2) {
+        for (i = n2; i <= n1; i++) {
+            finalSum += i;
+        }
+    }
+    else {
+        for (i = n1; i <= n2; i++) {
+            finalSum += i;
+        }
+    }
+    
+    return finalSum;
 };
 
 // Do not edit below this line
