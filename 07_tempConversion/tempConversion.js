3c0fe4d6b4706c798c2611e8999d1ea04b54bc20
Lincoln <163782652+ballooxn@users.noreply.github.com>
2024-05-27 17:30:36 -0500

    Complete Calculator

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(org) {
+  let newTemp = (org - 32) * (5/9);
+  return roundNumber(newTemp, 1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(org) {
+  let newTemp = (org * (9/5) + 32);
+  return roundNumber(newTemp, 1)
 };
+
+function roundNumber(number, digits) {
+  var multiple = Math.pow(10, digits);
+  var rndedNum = Math.round(number * multiple) / multiple;
+  return rndedNum;
+}
 
 // Do not edit below this line
 module.exports = {
