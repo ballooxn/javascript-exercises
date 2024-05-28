fdaa567216a8784dd9022ca799b93157846b2aea
Lincoln <163782652+ballooxn@users.noreply.github.com>
2024-05-27 20:19:39 -0500

    Finish Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+    str = str.toLowerCase();
+    const array = str.split("");
+    const filtered = array.filter((c) => isAlpha(c) || isNumeric(c));
+    const reversed = filtered.slice().reverse();
+    if (filtered.join("") == reversed.join("")) {
+        return true;
+    }
+    return false;
 };
+
+let isAlpha = function(ch){
+    return typeof ch === "string" && ch.length === 1
+        && (ch >= "a" && ch <= "z" || ch >= "A" && ch <= "Z");
+}
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
